17b0cfbf3b1299a38e88550a53437ce4d5618612
kichitano <ccespedes0793@gmail.com>
2020-06-27 20:55:09 -0500

    Subida 1 SGCAS

---
+++ PROYECTOSGCAS/src/Controller/ControllerCambiaPanel.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controller;
+
+/**
+ *
+ * @author RojeruSan
+ */
+import javax.swing.JPanel;
+
+public class ControllerCambiaPanel {
+    //
+    private JPanel container;
+    private JPanel content;
+
+
+    /**
+     * Constructor de clase
+     */
+    public ControllerCambiaPanel(JPanel container, JPanel content) {
+        this.container = container;
+        this.content = content;
+        this.container.removeAll();
+        this.container.revalidate();
+        this.container.repaint();
+        
+        this.container.add(this.content);
+        this.container.revalidate();
+        this.container.repaint();
+    }
+
+}//--> fin clase
